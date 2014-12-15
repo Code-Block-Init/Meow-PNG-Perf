d581b13c7601b0f5d59d236ef1ca86e8d75a91a5
Aswini S <ashumeow@live.com>
2014-12-15 16:57:14 +0530

    Update meow-png.js

--- meow-png.js
+++ meow-png.js
@@ -1,4 +1,4 @@
-  MeowPNG(function() {
+ var MeowPNG = (function() {
     "use strict";
     function MeowImagePlay() {
       Meow_CouleurFormat_Grey = 'G';
@@ -515,4 +515,28 @@
         new Meow_Fini();
       };
     }
+    var MeowImageCache = (function() {
+    var Meow_ImageCache = function() {
+      var Meow_ImageCache = [];
+      var Meow_CacheRoot = document.location.href.split('/');
+      Meow_CacheRoot.pop();
+      Meow_CacheRoot = Meow_CacheRoot.join('/') + '/';
+      Meow_Power.Meow_Push = function(src, Meow_LoadEvent) {
+        if (!src.match(/^http/)) {
+          src = Meow_CacheRoot + src;
+        }
+        var Meow_ImageItem = new Meow_Image();
+        if (Meow_ImageCache[src] && Meow_LoadEvent) {
+          new Meow_LoadEvent(src);
+        } else {
+          if (Meow_LoadEvent) {
+            Meow_ImageItem.Meow_OnLoad = Meow_LoadEvent;
+            Meow_ImageItem.Meow_OnError = Meow_LoadEvent;
+          }
+          Meow_ImageCache[src] = Meow_ImageItem;
+        }
+        Meow_ImageItem.src = src;
+      };
+    };
   });
+});
